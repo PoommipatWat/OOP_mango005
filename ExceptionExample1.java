06a416f2484f64fc88f02fb84aa948597a338b77
PoommipatWat <poommipat.wat@cmu.ac.th>
2023-01-17 23:13:53 +0700

    640610682

---
+++ ExceptionExample1.java
@@ -0,0 +1,10 @@
+public class ExceptionExample1 {
+    public static void main(String[] args) {
+      int a = 5, b = 0;
+      try {
+        int c = a / b;
+      } catch (ArithmeticException e) {
+        e.printStackTrace();
+      }
+    }
+  }
---
+++ StackOverflow.java
@@ -0,0 +1,8 @@
+class StackOverflow {
+    public static void test(int i) {
+      if (i == 0)
+        return;
+      else 
+        test(i++);
+    }
+}
